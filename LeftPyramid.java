effac831b0d695cbffd1f8de1f43da91c2c51c10
luckysharma-dev <luckyji1812@gmail.com>
2026-06-30 10:17:20 +0530

    patterns questions

---
+++ LeftPyramid.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LeftPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows for the left pyramid: ");
+        int rows = scanner.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}                                                       
